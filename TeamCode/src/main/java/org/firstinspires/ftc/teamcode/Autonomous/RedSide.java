070cd9aec8566fd34c322da913cbdf29fd9e2ed8
Nepteepan <eaplayford@gmail.com>
2024-11-23 08:16:12 -0500

    teaked atonmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedSide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedSide.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.TeleOp.TitanDriveAndArm;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -13,28 +14,47 @@ import com.qualcomm.robotcore.hardware.Servo;
 @Autonomous(name="RedSide")
 public class RedSide extends LinearOpMode {
 
+    public Servo leftClaw;
+    public Servo rightClaw;
     private DcMotor leftFront;
     private DcMotor rightFront;
     private DcMotor leftRear;
     private DcMotor rightRear;
+    private DcMotor elbow;
     private IMU imu;
     private int targetArmPosition = 0;
     final double INTAKE_COLLECT = -1.0;
     final double INTAKE_OFF = 0.0;
     final double INTAKE_DEPOSIT = 1.0;
+
     public DcMotor armMotor; //the arm motor
     public CRServo intake = null; //the active intake servo
     private void armStartupSequence() {
         armMotor = hardwareMap.get(DcMotor.class, "armMotor");
-        intake = hardwareMap.get(CRServo.class, "intake");
-        intake.setPower(INTAKE_OFF);
-        intake.setDirection(DcMotorSimple.Direction.FORWARD);
         armMotor.setTargetPosition(0);
         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        // armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        // armMotor.setTargetPosition((int) ARM_CLEAR_BARRIER);
         armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        armMotor.setTargetPosition(targetArmPosition);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armMotor.setPower(2.0);
+        sleep(500);
+
+        elbow = hardwareMap.get(DcMotor.class, "elbow");
+        elbow.setTargetPosition(0);
+        elbow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        elbow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        elbow.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        elbow.setDirection(DcMotorSimple.Direction.REVERSE);
+        sleep(500);
+        leftClaw = hardwareMap.get(Servo.class, "leftClaw");
+        leftClaw.setDirection(Servo.Direction.FORWARD);
+        leftClaw.setPosition(0);
+
+        rightClaw = hardwareMap.get(Servo.class, "rightClaw");
+        rightClaw.setDirection(Servo.Direction.REVERSE);
+        rightClaw.setPosition(0);
     }
     public void driveMotorInalazation(){
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
@@ -42,6 +62,18 @@ public class RedSide extends LinearOpMode {
         leftRear = hardwareMap.get(DcMotor.class, "leftRear");
         rightRear = hardwareMap.get(DcMotor.class, "rightRear");
         imu = hardwareMap.get(IMU.class, "imu");
+    }
+
+    private void openClaw() {
+        leftClaw.setPosition(0.5);
+        rightClaw.setPosition(0.5);
+        //_clawStatus = TitanDriveAndArm.clawStatus.OPEN;
+    }
+
+    private void closeClaw() {
+        leftClaw.setPosition(0);
+        rightClaw.setPosition(0);
+        //_clawStatus = TitanDriveAndArm.clawStatus.CLOSED;
     }
     public void moveNumberOfInches(double numberOfInches, double power) {
         double totalRevs = 42 * numberOfInches;
@@ -77,15 +109,15 @@ public class RedSide extends LinearOpMode {
 
         waitForStart();
         while (opModeIsActive()){
-            armMotor.setTargetPosition(100);
+            armMotor.setTargetPosition(200);
             armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             armMotor.setPower(1);
             sleep(500);
             moveNumberOfInches(68, 1);
             sleep(500);
-            intake.setPower(INTAKE_DEPOSIT);
+            openClaw();
             sleep(2000);
-            intake.setPower(INTAKE_OFF);
+            closeClaw();
             sleep(500);
             moveNumberOfInches(-10, 1);
             sleep(500);
